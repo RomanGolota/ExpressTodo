3ae05d18dd0eb294e76de3a8c13061ee627f21ed
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-01-05 01:45:35 +0200

    Refactoring registerAuthController and tests

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,101 +1,66 @@
-import bcrypt from "bcryptjs";
-import registerAuthService from "../service/registerAuthService.js";
-import registerAuthController from "../controller/registerAuthController.js";
+import RegisterAuthController from "../controller/registerAuthController.js";
+import {beforeEach} from "@jest/globals";
 
-describe('login', () => {
-    describe('These tests must pass successfully', () => {
-        it('returns a token when called with correct login credentials', async () => {
-            // Create a mock request object
-            const req = {
-                body: {
-                    email: 'test@example.com',
-                    password: 'password',
-                },
-            };
+describe('registerAuthController', () => {
+    let req
+    let res
+    let registerAuthService
+    let registerAuthController
+    let tokenCreator
 
-            // Create a mock response object
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
+    beforeEach(() => {
+        req = {
+            body: {
+                email: 'test@email.com',
+                password: 'password'
+            }
+        }
+        res = {
+            status: jest.fn(() => res),
+            json: jest.fn(() => res)
+        }
+        registerAuthService = {
+            findUser: jest.fn(),
+            createUser: jest.fn(),
+        };
+        tokenCreator = {
+            checkPassword: jest.fn(),
+            getToken: jest.fn(),
+            hashPassword: jest.fn()
+        }
+        registerAuthController = new RegisterAuthController(registerAuthService, tokenCreator);
+    })
 
-            // Mock the findUser function to return a user object
-            registerAuthService.findUser = jest.fn().mockResolvedValue({
-                id: 1,
-                email: 'test@example.com',
-                password: bcrypt.hashSync('password', 10),
-            });
-
-            // Call the login function with the mock request and response objects
+    describe('login', () =>{
+        it('should return a 400 error if the request body is invalid', async () => {
+            req.body.email = 'invalid';
             await registerAuthController.login(req, res);
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "User with this email does not exist" });
+        });
 
-            // Assert that the function returned a token in the response
+        it('should return a 400 error if password is short', async () => {
+            req.body.password = 'pas';
+            await registerAuthController.login(req, res);
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "User with this email does not exist" });
+        });
+
+        it('should return a 400 error if user not found', async () => {
+            registerAuthService.findUser.mockResolvedValue(false);
+            await registerAuthController.login(req, res);
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: 'User with this email does not exist' });
+        });
+
+        it('should return a 200 status code and token if user found', async () => {
+            registerAuthService.findUser.mockResolvedValue({});
+            tokenCreator.checkPassword.mockResolvedValue(true)
+            tokenCreator.getToken.mockResolvedValue({token: 'token'})
+
+            await registerAuthController.login(req, res);
             expect(res.status).toHaveBeenCalledWith(200);
-            expect(res.json).toHaveBeenCalledWith({
-                token: expect.any(String),
-            });
+            expect(res.json).toHaveBeenCalledWith({ token: expect.anything() });
         });
     })
-
-    describe('These tests must be failed', () => {
-        it('returns an error message when called with incorrect login credentials', async () => {
-            // Create a mock request object with incorrect login credentials
-            const req = {
-                body: {
-                    email: 'test@example.com',
-                    password: 'incorrect',
-                },
-            };
-
-            // Create a mock response object
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
-
-            // Mock the findUser function to return a user object
-            registerAuthService.findUser = jest.fn().mockResolvedValue({
-                id: 1,
-                email: 'test@example.com',
-                password: bcrypt.hashSync('password', 10),
-            });
-
-            // Call the login function with the mock request and response objects
-            await registerAuthController.login(req, res);
-
-            // Assert that the function returned an error message in the response
-            expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.json).toHaveBeenCalledWith({
-                message: 'Incorrect password',
-            });
-        });
-
-        it('returns an error message when called with a non-existent user', async () => {
-            // Create a mock request object with a non-existent email
-            const req = {
-                body: {
-                    email: 'non-existent@example.com',
-                    password: 'password',
-                },
-            };
-
-            // Create a mock response object
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
-
-            // Mock the findUser function to return null
-            registerAuthService.findUser = jest.fn().mockResolvedValue(null);
-
-            // Call the login function with the mock request and response objects
-            await registerAuthController.login(req, res);
-
-            // Assert that the function returned an error message in the response
-            expect(res.status).toHaveBeenCalledWith(400);
-            expect(res.json).toHaveBeenCalledWith({
-                message: 'User with this email does not exist',
-            });
-        });
-    })
-});
+})
--- src/controller/registerAuthController.js
+++ src/controller/registerAuthController.js
@@ -1,59 +1,45 @@
-import bcrypt from 'bcryptjs';
-import Joi from '@hapi/joi';
-import jsonwebtoken from 'jsonwebtoken';
-import registerAuthService from '../service/registerAuthService.js';
 import dotenv from "dotenv";
 dotenv.config();
 
-class registerAuthController {
-    constructor(registerAuthService) {
+export default class registerAuthController {
+    constructor(registerAuthService, tokenCreator) {
         this.registerAuthService = registerAuthService;
+        this.tokenCreator = tokenCreator
     }
 
     register = async (req, res) => {
         try {
-            const schema = Joi.object({
-                email: Joi.string().email().required(),
-                password: Joi.string().min(4).max(255).required()
-            });
-            const { error } = schema.validate(req.body);
-            if (error) {
-                return res.status(400).json({ error: error.details });
-            }
-
-            const {email, password} = req.body;
+            const { email, password } = req.body;
             const candidate = await this.registerAuthService.findUser(email);
             if (candidate) {
-                return res.status(400).json({message: 'User with this email already exists'});
+                return res.status(400).json({ message: 'User with this email already exists' });
             }
-            const hashPassword = await bcrypt.hash(password, 7);
-            await registerAuthService.createUser(email, hashPassword);
-            return res.status(200).json({message: `User ${email} was created`});
+            const hashPassword = await this.tokenCreator.hashPassword(password, 7)
+            await this.registerAuthService.createUser(email, hashPassword);
+            return res.status(200).json({ message: `User ${email} was created` });
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ message: 'Internal server error' });
         }
-        catch (e) {
-            console.log(e)
-        }
-    }
+    };
 
     login = async (req, res) => {
         try {
-            console.log(this)
-            const {email, password} = req.body;
+            const { email, password } = req.body;
             const user = await this.registerAuthService.findUser(email);
 
             if (!user) {
-                return res.status(400).json({message: 'User with this email does not exist'});
+                return res.status(400).json({ message: 'User with this email does not exist' });
             }
-            const validPassword = bcrypt.compareSync(password, user.password);
+            const validPassword = this.tokenCreator.checkPassword(password, user.password);
             if (!validPassword) {
-                return res.status(400).json({message: 'Incorrect password'});
+                return res.status(400).json({ message: 'Incorrect password' });
             }
-            const token = jsonwebtoken.sign({id: user.id}, process.env.SECRET_KEY, {expiresIn: '1h'});
-            return res.status(200).json({token});
+            const token = this.tokenCreator.getToken({ id: user.id })
+            return res.status(200).json({ token });
         } catch (e) {
-            console.log(e)
+            console.log(e);
+            return res.status(500).json({ message: 'Internal server error' });
         }
-    }
+    };
 }
-
-export default registerAuthController;
