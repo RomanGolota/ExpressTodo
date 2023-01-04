02c2eb37bcc921f652c69510f668f80cd1de522d
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-01-04 15:13:00 +0200

    Add dependency injection in registerAuthController

--- src/router/registerAuthRouter.js
+++ src/router/registerAuthRouter.js
@@ -1,7 +1,10 @@
 import {Router} from "express";
-import registerAuthController from "../controller/registerAuthController.js";
+import RegisterAuthController from "../controller/registerAuthController.js";
+import RegisterAuthService from "../service/registerAuthService.js";
 
 const router = Router();
+const registerAuthService = new RegisterAuthService()
+const registerAuthController = new RegisterAuthController(registerAuthService)
 
 router.post('/register', registerAuthController.register);
 router.post('/login', registerAuthController.login);
