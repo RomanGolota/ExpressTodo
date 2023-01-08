aa4e6c2769293e0f74e7230b3a3d523465f640d6
GolotaRoman91 <93368048+GolotaRoman91@users.noreply.github.com>
2023-01-08 18:03:49 +0200

    Add tests for todosCRUD

---
+++ __tests__/getTodos.test.js
@@ -0,0 +1,38 @@
+import TodosController from '../src/controller/todosController.js';
+
+describe('TodosController', () => {
+    let todosController;
+    let todosService;
+
+    beforeEach(() => {
+        todosService = {
+            getTodos: jest.fn(),
+        };
+        todosController = new TodosController(todosService);
+    });
+
+    describe('getTodos', () => {
+        it('should return the todos for the given user', async () => {
+            const req = { user: { id: 123 } };
+            const res = { send: jest.fn() };
+            todosService.getTodos.mockResolvedValue([]);
+
+            await todosController.getTodos(req, res);
+
+            expect(todosService.getTodos).toHaveBeenCalledWith(123);
+            expect(res.send).toHaveBeenCalledWith([]);
+        });
+
+        it('should return a 500 error if there is a problem retrieving the todos', async () => {
+            const req = { user: { id: 123 } };
+            const res = { status: jest.fn(() => res), json: jest.fn() };
+            todosService.getTodos.mockRejectedValue(new Error('Error getting todos'));
+
+            await todosController.getTodos(req, res);
+
+            expect(todosService.getTodos).toHaveBeenCalledWith(123);
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({ message: 'Error getting todos' });
+        });
+    });
+});
---
+++ __tests__/updateTodo.test.js
@@ -0,0 +1,40 @@
+import TodosController from '../src/controller/todosController.js';
+
+describe('TodosController', () => {
+    let todosController;
+    let todosService;
+
+    beforeEach(() => {
+        todosService = {
+            updateTodo: jest.fn(),
+        };
+        todosController = new TodosController(todosService);
+    });
+
+    describe('updateTodo', () => {
+        it('should update the todo with the given id', async () => {
+            const req = {params: {id: '123'}, body: {text: 'Learn NestJS'}};
+            const res = {status: jest.fn(() => res), json: jest.fn()};
+            todosService.updateTodo.mockResolvedValue(true);
+
+            await todosController.updateTodo(req, res);
+
+            expect(todosService.updateTodo).toHaveBeenCalledWith('123', {text: 'Learn NestJS'});
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({message: 'Todo updated successfully'});
+        });
+
+        it('should return a 404 error if the todo is not found', async () => {
+            const req = {params: {id: '123'}, body: {text: 'Learn Jest'}};
+            const res = {status: jest.fn(() => res), json: jest.fn()};
+            todosService.updateTodo.mockResolvedValue(false);
+
+            await todosController.updateTodo(req, res);
+
+            expect(todosService.updateTodo).toHaveBeenCalledWith('123', {text: 'Learn Jest'});
+            expect(res.status).toHaveBeenCalledWith(404);
+            expect(res.json).toHaveBeenCalledWith({message: 'Todo not found'});
+        });
+    });
+});
+
